fa88910e1c3eb8c1a30fe5fc732049d481e4d52b
Ali Thompson <alithom123@gmail.com>
2019-05-13 00:22:44 -0500

    routes to test the trial api are working and receiving data. Used postman to test.

--- routes/api/tests.js
+++ routes/api/tests.js
@@ -5,12 +5,9 @@ const { apiBaseUrl} = require("../../config/keys.js");
 
 console.log(apiBaseUrl);
 
-// @route POST api/users/register
-// @desc Register user
-// @access Public
 router.get("/get-trails-by-lat-long", (req, res) => {
 
-    console.log("in route");
+    console.log("in trails by lat long route");
 
     axios.get(`${apiBaseUrl}/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200466516-8529d615652675afe116280247200513`)
         .then( response => {
@@ -23,22 +20,45 @@ router.get("/get-trails-by-lat-long", (req, res) => {
             console.log("err ", err);
             // res.json(err);
         });
+});
 
 
-    // https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200466516-8529d615652675afe116280247200513
+router.get("/get-trails-by-id", (req, res) => {
 
-    // const axios = require('axios');
-    //
-    // axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    //     .then(response => {
-    //         console.log(response.data.url);
-    //         console.log(response.data.explanation);
-    //     })
-    //     .catch(error => {
-    //         console.log(error);
-    //     });
+    console.log("in trails by id route");
 
-    });
+    // https://www.hikingproject.com/data/get-trails-by-id?ids=7001635,7002742,7000108,7002175,7005207&key=200466516-8529d615652675afe116280247200513
+    axios.get(`${apiBaseUrl}/data/get-trails-by-id?ids=7001635,7002742,7000108,7002175,7005207&key=200466516-8529d615652675afe116280247200513`)
+        .then(response => {
+            // console.log("response ", response);
+            console.log("success");
+            res.json(response.data);
+        })
+        .catch(err => {
+            // console.log("error");
+            console.log("err ", err);
+            // res.json(err);
+        });
+});
+
+
+router.get("/get-conditions-by-id", (req, res) => {
+
+        console.log("in conditions by id route");
+
+        // https://www.hikingproject.com/data/get-conditions?ids=7001635,7002742,7000108,7002175,7005207&key=200466516-8529d615652675afe116280247200513
+        axios.get(`${apiBaseUrl}/data/get-conditions?ids=7001635,7002742,7000108,7002175,7005207&key=200466516-8529d615652675afe116280247200513`)
+            .then(response => {
+                // console.log("response ", response);
+                console.log("success");
+                res.json(response.data);
+            })
+            .catch(err => {
+                // console.log("error");
+                console.log("err ", err);
+                // res.json(err);
+            });
+});
 
 
 module.exports = router;
