28858530d23df11c43ed9d5700e67fb3c4e94bd1
Ali Thompson <alithom123@gmail.com>
2019-05-25 15:32:31 -0400

    added a schema for a "Hike" which includes comments.  Added an api route to create a Hike. Added an api route that posts a comment to a hike. The comments exists as an array on the Hike.

--- routes/api/hikes.js
+++ routes/api/hikes.js
@@ -1,21 +1,84 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
-const passport = require("passport");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+// const keys = require("../../config/keys");
+// const passport = require("passport");
 const trails = require("../../config/seedTrails");
 
 // Load input validation
-const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
+// const validateRegisterInput = require("../../validation/register");
+// const validateLoginInput = require("../../validation/login");
 
 // Load User model
 // const User = require("../../models/User");
 
-// @route GET api/trails
-// @desc Get all trails in database
-// @access Public
+// Load Hikes model
+const Hike = require("../../models/Hike");
+
+
+router.get("/", (req, res) => {
+
+
+        Hike.find({}).then(hikes => {
+
+                return res.json(hikes);
+        });
+});
+
+
+router.post("/", (req, res) => {
+
+        console.log(`req.body = %o`, req.body);
+
+        const newHike = new Hike({
+                ...req.body
+        });
+
+
+        // const newHike = new Hike({
+        //         ascent: 1000,
+        //         conditionDate: '10/01/1982',
+        //         conditionDetails: 'Heavy rainfall makes river crossing dangerous.',
+        //         conditionStatus: 'Dangerous',
+        //         descent: 500,
+        //         difficulty: 'Very Hard',
+        //         high: 10000,
+        //         apiId: 1234567890,
+        //         imgMedium: '',
+        //         imgSmall: '',
+        //         imgSmallMed: '',
+        //         imgSqSmall: '',
+        //         latitude: 0.0,
+        //         length: 2.5,
+        //         location: 'Austin, TX',
+        //         longitude: 0.0,
+        //         low: 0,
+        //         name: 'Greenbelt',
+        //         starVotes: 120,
+        //         stars: 4.5,
+        //         summary: 'A great first trail to get your feet wet in Austin',
+        //         type: '?',
+        //         url: '',
+        //         comments: [{ "text": "comment 1"}, { "text": "comment2"}]
+        // });
+
+        newHike
+            .save()
+            .then(hike => res.json(hike))
+            .catch(err => console.log(`Error in post hike api call: %o`, err));
+
+        });
+
+
+router.post("/comment", (req, res) => {
+
+        // Find one by Id, Then do an update
+        Hike.findOneAndUpdate({_id: req.body.hikeId}, {$push: {comments: { "text": req.body.comment}}})
+            .then(hike => res.json(hike))
+            .catch(err => console.log(`Error in post comment api call: %o`, err));
+});
+
 router.get("/all", (req, res) => {
     // Form validation
 
@@ -66,6 +129,32 @@ router.get("/comment", (req, res) => {
 });
 
 
-
-
 module.exports = router;
+
+
+/* POSTMAN JSON FOR CREATE HIKE */
+// {
+//         "ascent": 1000,
+//     "conditionDate": "10/01/1982",
+//     "conditionDetails": "Heavy rainfall makes river crossing dangerous.",
+//     "conditionStatus": "Dangerous",
+//     "descent": 500,
+//     "difficulty": "Very Hard",
+//     "high": 10000,
+//     "apiId": 1234567890,
+//     "imgMedium": "",
+//     "imgSmall": "",
+//     "imgSmallMed": "",
+//     "imgSqSmall": "",
+//     "latitude": 0.0,
+//     "length": 2.5,
+//     "location": "Austin, TX",
+//     "longitude": 0.0,
+//     "low": 0,
+//     "name": "Greenbelt",
+//     "starVotes": 120,
+//     "stars": 4.5,
+//     "summary": "A great first trail to get your feet wet in Austin",
+//     "type": "?",
+//     "url": ""
+// }
