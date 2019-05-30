4ccbf5cce1350c77b14d8a08bb63a95e2bbd332f
mpartee123 <m_partee@hotmail.com>
2019-05-29 19:06:22 -0500

    added .env.deployment to resolve host error

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-if (process.env.NODE_ENV === "production") {
-    // Express will serve up production assets
-    app.use(express.static("build"));
-
-    // Express will serve up the front-end index.html file if it doesn't recognize the route
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve("build", "index.html"))
-    );
-}
-
 // Choose the port and start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
